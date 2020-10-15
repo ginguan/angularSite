83aea297aeada3e0a5f7bdbbd1880827833a7937
ginguan <yguan074@uottawa.ca>
2020-10-14 23:57:34 -0400

    generate page

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListingsPageComponent } from './listings-page/listings-page.component';
+import { ListingsDetailPageComponent } from './listings-detail-page/listings-detail-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
+import { NewListingsPageComponent } from './new-listings-page/new-listings-page.component';
+import { EditListingsPageComponent } from './edit-listings-page/edit-listings-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListingsPageComponent,
+    ListingsDetailPageComponent,
+    ContactPageComponent,
+    MyListingsPageComponent,
+    NewListingsPageComponent,
+    EditListingsPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/edit-listings-page/edit-listings-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-listings-page',
+  templateUrl: './edit-listings-page.component.html',
+  styleUrls: ['./edit-listings-page.component.css']
+})
+export class EditListingsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
