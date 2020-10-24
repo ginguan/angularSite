fa823e7bdbdf7621b05deb2c40c0dccb302cdc3f
ginguan <yguan074@uottawa.ca>
2020-10-24 16:26:31 -0400

    update

--- frontend/src/app/edit-listings-page/edit-listings-page.component.ts
+++ frontend/src/app/edit-listings-page/edit-listings-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import {Listing} from "../types";
-import {fakeListings} from "../fake-data";
+import {fakeListings, fakeMyListings} from "../fake-data";
 
 @Component({
   selector: 'app-edit-listings-page',
@@ -10,13 +10,23 @@ import {fakeListings} from "../fake-data";
 })
 export class EditListingsPageComponent implements OnInit {
 
+  listing :Listing;
   constructor(
     private route:ActivatedRoute,
     private router:Router) { }
 
   ngOnInit(): void {
     //https://angular.io/api/router/ActivatedRouteSnapshot
-    const id  = this.route.snapshot.paramMap.get('id');
+    //access para : route.paramMap.subscribe if you need to update the URL parameter in the same component
+    /*
+    ngOnInit() {
+      this.route.paramMap.subscribe(params => {
+      this.car = params.get("carBrand")
+  })}
+     */
+    const id  = this.route.snapshot.paramMap.get('id'); //only get the id
+    this.listing= fakeMyListings.find(listing => listing.id ===id);
+    
     
   }
   onSubmit():void{
